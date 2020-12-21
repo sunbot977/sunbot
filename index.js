60240bcfcc890106515f2b0ec476ca84e4ca306c
sunbot977 <cjsdntjr5@gmail.com>
2020-12-21 15:48:48 +0900

    index.js

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ client.on("message", (message) => {
       .setFooter("짱구", img2)
 
     message.channel.send(embed)
-    await message.channel.delete()
   } else if (message.content == "?명령어") {
     let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
     let commandList = [
